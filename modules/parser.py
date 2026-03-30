c92bcaee9daf1ab3b5ad822d2ea846f50e1b70ab
haalasz <18116421+haalasz@users.noreply.github.com>
2026-03-30 16:28:05 +0200

    add explicit help flag and clearer CLI argument setup

--- modules/parser.py
+++ modules/parser.py
@@ -7,8 +7,27 @@ class CustomArgumentParser(argparse.ArgumentParser):
         return f"{LOGO.strip()}\n\n{default_help}"
 
 def create_parser():
-    parser = CustomArgumentParser(description="QPPL (Quick Phage PipeLine) help menu:")
-    parser.add_argument("-v", "--version", action="store_true", help="Display QPPL version and logo")
-    parser.add_argument("-gc", "--generate-config", action="store_true", help="Generate a default configuration file (overrides existing)")
-    parser.add_argument("-c", "--config", type=str, default='qppl.conf', help="Path to the configuration file")
+    parser = CustomArgumentParser(description="QPPL (Quick Phage PipeLine) help menu:", add_help=False)
+
+    parser.add_argument(
+        "-h", "--help",
+        action="help",
+        help="Show this help message and exit"
+    )
+    parser.add_argument(
+        "-v", "--version",
+        action="store_true",
+        help="Display QPPL version and logo"
+    )
+    parser.add_argument(
+        "-gc", "--generate-config",
+        action="store_true",
+        help="Generate a default configuration file (overrides existing)"
+    )
+    parser.add_argument(
+        "-c", "--config",
+        type=str,
+        default='qppl.conf',
+        help="Path to the configuration file"
+    )
     return parser
