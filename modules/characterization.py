46a51a3e38deb5d8722e36355a27a11f35d2251d
haalasz <18116421+haalasz@users.noreply.github.com>
2026-04-08 15:50:03 +0200

    reshape function table and join depth/coverage

--- modules/characterization.py
+++ modules/characterization.py
@@ -118,11 +118,30 @@ def generate_commands(characterization_dir, output_dir, file, task_params, basen
         6: [ # INTERMEDIATE FILES
             rf"""awk 'BEGIN {{FS=OFS=","}} NR==1 {{$1="contig"; $2="vclust_cluster_id"; $NF="repres_score"; for (i=3; i<=NF-1; i++) $i="checkv_"$i}} 1' {assembly_dir}/out_vclust/{basename}/{basename}_selected_genomes.csv | sed 's/,/\t/g' > {assembly_dir}/out_vclust/{basename}/{basename}_selected_genomes_final.tsv""",
             #
-            rf"sort -t $'\t' -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density.tsv > {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv",
-            rf"sort -t $'\t' -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared.tsv > {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv",
-            rf"join -t $'\t' -1 1 -2 1 {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv > {characterization_dir}/out_pharokka/{basename}/{basename}_merged.tsv",
-            rf"""awk 'BEGIN {{FS=OFS="\t"}} NR==1 {{$1="contig"; for (i=2; i<=NF; i++) $i="pharokka_"$i}} 1' {characterization_dir}/out_pharokka/{basename}/{basename}_merged.tsv > {characterization_dir}/out_pharokka/{basename}/{basename}_pharokka_final.tsv""",
-            rf"rm {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_merged.tsv",
+            rf"""sort -t $'\t' -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density.tsv | awk 'BEGIN {{FS=OFS="\t"}} NR==1 {{$1="contig"; for (i=2; i<=NF; i++) $i="pharokka_"$i}} 1' > {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv""",
+            rf"""sort -t $'\t' -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared.tsv | awk 'BEGIN {{FS=OFS="\t"}} NR==1 {{$1="contig"; for (i=2; i<=NF; i++) $i="pharokka_closest_"$i}} 1' > {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv""",
+            rf"""{{
+            printf "contig\tpharokka_CDS\tpharokka_tRNAs\tpharokka_tmRNAs\tpharokka_CRISPRs\n";
+            awk 'BEGIN {{FS=OFS="\t"}}
+                NR==1 {{next}}
+                {{
+                    d=$1; c=$2+0; ctg=$3;
+                    if (d=="CDS") cds[ctg]=c;
+                    else if (d=="tRNAs") trnas[ctg]=c;
+                    else if (d=="tmRNAs") tmrnas[ctg]=c;
+                    else if (d=="CRISPRs") crisprs[ctg]=c;
+                }}
+                END {{
+                    for (ctg in cds) seen[ctg]=1;
+                    for (ctg in trnas) seen[ctg]=1;
+                    for (ctg in tmrnas) seen[ctg]=1;
+                    for (ctg in crisprs) seen[ctg]=1;
+                    for (ctg in seen)
+                    print ctg, (ctg in cds?cds[ctg]:0), (ctg in trnas?trnas[ctg]:0), (ctg in tmrnas?tmrnas[ctg]:0), (ctg in crisprs?crisprs[ctg]:0);
+                }}' {characterization_dir}/out_pharokka/{basename}/{basename}_cds_functions.tsv | sort -t $'\t' -k1,1;
+            }} > {characterization_dir}/out_pharokka/{basename}/{basename}_cds_functions_wide.tsv""",
+            rf"join -t $'\t' -1 1 -2 1 {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_cds_functions_wide.tsv | join -t $'\t' -1 1 -2 1 - {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv > {characterization_dir}/out_pharokka/{basename}/{basename}_final.tsv",
+            rf"rm {characterization_dir}/out_pharokka/{basename}/{basename}_length_gc_cds_density_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_top_hits_mash_inphared_sorted.tsv {characterization_dir}/out_pharokka/{basename}/{basename}_cds_functions_wide.tsv",
             #
             rf"""cut -f2- {characterization_dir}/out_taxmyphage/{basename}/Summary_taxonomy.tsv | awk 'BEGIN {{FS=OFS="\t"}} NR==1 {{$1="contig"; for (i=2; i<=NF; i++) $i="taxmyph_"$i}} 1' > {characterization_dir}/out_taxmyphage/{basename}/{basename}_taxmyphage_final.tsv""",
             #
@@ -132,8 +151,8 @@ def generate_commands(characterization_dir, output_dir, file, task_params, basen
             #
             rf"""awk 'FNR==NR && /^>/ {{sub(/^>/, "", $1); sub(/ .*/, "", $1); contigs[$1]=0; next}} FNR>1 {{if ($2 in contigs) {{if (FILENAME ~ "card") card[$2]++; else if (FILENAME ~ "megares") megares[$2]++; else if (FILENAME ~ "ncbi") ncbi[$2]++; else if (FILENAME ~ "vfdb") vfdb[$2]++}}}} END {{print "contig\t", "abricate_card_hits\t", "abricate_megares_hits\t", "abricate_ncbi_hits\t", "abricate_vfdb_hits"; for (contig in contigs) printf "%s\t%d\t%d\t%d\t%d\n", contig, (card[contig] ? card[contig] : 0), (megares[contig] ? megares[contig] : 0), (ncbi[contig] ? ncbi[contig] : 0), (vfdb[contig] ? vfdb[contig] : 0)}}' {assembly_dir}/out_vclust/{basename}/{basename}_selected_genomes.fasta {characterization_dir}/out_abricate/{basename}/card_{basename}.abr {characterization_dir}/out_abricate/{basename}/megares_{basename}.abr {characterization_dir}/out_abricate/{basename}/ncbi_{basename}.abr {characterization_dir}/out_abricate/{basename}/vfdb_{basename}.abr > {characterization_dir}/out_abricate/{basename}/{basename}_abricate_final.tsv""",
             #
-            rf"""join -t $'\t' <(sort -k1,1 {characterization_dir}/out_phabox/{basename}/{basename}_phabox_final.tsv) <(sort -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_pharokka_final.tsv) | join -t $'\t' - <(sort -k1,1 {assembly_dir}/out_vclust/{basename}/{basename}_selected_genomes_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_abricate/{basename}/{basename}_abricate_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_vhulk/{basename}/{basename}_vHULK_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_taxmyphage/{basename}/{basename}_taxmyphage_final.tsv) | awk -v sample="{basename}" 'BEGIN {{OFS=FS="\t"}} NR==1 {{print "sample", $0}} NR>1 {{print sample, $0}}' > {characterization_dir}/intermediate/{basename}_characterization_tmp.tsv""",
-            rf"""awk -F'\t' 'NR==1{{print $0 "\tpredicted_genus\tpredicted_species"; next}} {{delete count; if($85!="")count[$85]++; if($56!="")count[$56]++; if($33!="")count[$33]++; predicted_genus=""; max_count=0; for(val in count) if(count[val]>max_count){{predicted_genus=val; max_count=count[val]}} if(max_count<=1) predicted_genus=""; predicted_species=""; if($88!=""&&$34!=""&&$88==$34) predicted_species=$88; print $0 "\t" predicted_genus "\t" predicted_species}}' {characterization_dir}/intermediate/{basename}_characterization_tmp.tsv > {characterization_dir}/intermediate/{basename}_characterization_full.tsv""",
+            rf"""join -t $'\t' <(sort -k1,1 {characterization_dir}/out_phabox/{basename}/{basename}_phabox_final.tsv) <(sort -k1,1 {characterization_dir}/out_pharokka/{basename}/{basename}_final.tsv) | join -t $'\t' - <(sort -k1,1 {assembly_dir}/out_vclust/{basename}/{basename}_selected_genomes_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_abricate/{basename}/{basename}_abricate_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_vhulk/{basename}/{basename}_vHULK_final.tsv) | join -t $'\t' - <(sort -k1,1 {characterization_dir}/out_taxmyphage/{basename}/{basename}_taxmyphage_final.tsv) | join -t $'\t' - <(sort -k1,1 {assembly_dir}/depth_coverage/{basename}_aln.tsv) | awk -v sample="{basename}" 'BEGIN {{OFS=FS="\t"}} NR==1 {{print "sample", $0}} NR>1 {{print sample, $0}}' > {characterization_dir}/intermediate/{basename}_characterization_tmp.tsv""",
+            rf"""awk -F'\t' 'NR==1{{print $0 "\tpredicted_genus\tpredicted_species"; next}} {{delete count; if($89!="")count[$89]++; if($60!="")count[$60]++; if($33!="")count[$33]++; predicted_genus=""; max_count=0; for(val in count) if(count[val]>max_count){{predicted_genus=val; max_count=count[val]}} if(max_count<=1) predicted_genus=""; predicted_species=""; if($92!=""&&$34!=""&&$92==$34) predicted_species=$92; print $0 "\t" predicted_genus "\t" predicted_species}}' {characterization_dir}/intermediate/{basename}_characterization_tmp.tsv > {characterization_dir}/intermediate/{basename}_characterization_full.tsv""",
             rf"""rm {characterization_dir}/intermediate/{basename}_characterization_tmp.tsv"""
         ],
         7: [ # RESULTS
@@ -141,7 +160,7 @@ def generate_commands(characterization_dir, output_dir, file, task_params, basen
             rf"mkdir -p {output_dir}/results/genomes/{basename}",
             rf"seqkit replace -p 'rotated.*' -r '' {characterization_dir}/out_pharokka/{basename}/{basename}_dnaapler_reoriented.fasta | seqkit seq -w 0 | seqkit split -i --by-id-prefix '{basename}_' -O {output_dir}/results/genomes/{basename}/",
             #
-            rf"""awk -F'\t' '{{print $1","$2","$35","$36","$50","$38","$51","$52","$53","$55","$4","$12","$81","$82","$83","$84","$91","$92","$93","$94","$95","$96","$97","$98","$99","$102","$103}}' {characterization_dir}/intermediate/{basename}_characterization_full.tsv > {output_dir}/results/{basename}_final_results.csv"""
+            rf"""awk -F'\t' '{{print $1","$2","$35","$36","$38","$39","$40","$41","$42","$110","$111","$106","$107","$109","$108","$112","$113","$75","$76","$77","$78","$79","$80","$81","$82","$83","$4","$12","$85","$86","$87","$88","$95","$96","$97","$98","$99","$100","$101","$102","$103","$114","$115}}' {characterization_dir}/intermediate/{basename}_characterization_full.tsv > {output_dir}/results/{basename}_final_results.csv"""
         ]
     }
     return commands_by_step.get(step_number, [])
